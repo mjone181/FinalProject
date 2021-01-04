75032acb2c66e3d9f581c3f3684bfbb7452bf8c1
mjone181 <mjone181@gmail.com>
2021-01-04 14:50:13 -0600

    Installed Identity Samples to the UI Layer of FinalProject.

--- FinalProject.UI.MVC/Global.asax
+++ FinalProject.UI.MVC/Global.asax
@@ -1,1 +1,1 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FinalProject.UI.MVC.MvcApplication" Language="C#" %>
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="IdentitySample.MvcApplication" Language="C#" %>
--- FinalProject.UI.MVC/Startup.cs
+++ FinalProject.UI.MVC/Startup.cs
@@ -1,8 +1,6 @@
-﻿using Microsoft.Owin;
-using Owin;
+﻿using Owin;
 
-[assembly: OwinStartupAttribute(typeof(FinalProject.UI.MVC.Startup))]
-namespace FinalProject.UI.MVC
+namespace IdentitySample
 {
     public partial class Startup
     {
