2047762e5c5be2b54d416e3f138934e065b01b41
mjone181 <mjone181@gmail.com>
2021-01-06 09:53:27 -0600

    Completed inital scaffolding for all the Tables and created an Index and Create View for each.

--- FinalProject.UI.MVC/Controllers/LocationsController.cs
+++ FinalProject.UI.MVC/Controllers/LocationsController.cs
@@ -3,15 +3,65 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+//Getting access to the Models to make this Controller work
+using FinalProject.UI.MVC.Models;
+//Accessing the Data Layer to bring our Data into the UI Layer
+using FinalProject.DATA.EF;
 
 namespace FinalProject.UI.MVC.Controllers
 {
     public class LocationsController : Controller
     {
+        //Create a new Database Keyword
+        FinalProjectEntities db = new FinalProjectEntities();
+
         // GET: Locations
         public ActionResult Index()
         {
+            var locations = (from l in db.UserDetails
+                               select l).ToList();
+            return View(locations);
+        }
+
+        public ActionResult Locations()
+        {
+            //Link to SQL Database with UserDetailsViewModel
+            List<LocationsViewModel> locations = db.Locations.
+                Select(l => new LocationsViewModel()
+                {
+                   LocationId = l.LocationId,
+                   LocationName = l.LocationName,
+                   Address = l.Address,
+                   City = l.City,
+                   State = l.State,
+                   ZipCode = l.ZipCode,
+                   ReservationLimit = l.ReservationLimit    
+                }).ToList<LocationsViewModel>();
             return View();
+        }
+
+        //GET
+        public ActionResult Create()
+        {
+            ViewBag.UserId = new SelectList(db.Locations, "LoactionId", "Locations");
+            return View();
+        }
+
+        //POST: Locations
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "LocationId, LocationName, Address, City, State, ZipCode, ReservationLimit")] Location location)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Locations.Add(location);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            //Grabbing the newly entered data and showing it to the screen.
+            ViewBag.OwnerAssetId = new SelectList(db.Locations, "LocationId", "LocationId", location.LocationId);
+            return View(location);
         }
     }
 }
--- FinalProject.UI.MVC/Controllers/UserDetailsController.cs
+++ FinalProject.UI.MVC/Controllers/UserDetailsController.cs
@@ -1,17 +1,64 @@
-﻿using System;
+﻿//Accessing the Data Layer to bring our Data into the UI Layer
+using FinalProject.DATA.EF;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+//Getting access to the Models to make this Controller work
+using FinalProject.UI.MVC.Models;
 
 namespace FinalProject.UI.MVC.Controllers
 {
     public class UserDetailsController : Controller
     {
+        //Create a new Database Keyword
+        FinalProjectEntities db = new FinalProjectEntities();
+        
         // GET: UserDetails
         public ActionResult Index()
         {
+            var userDetails = (from u in db.UserDetails
+                               select u).ToList();
+            return View(userDetails);
+        }
+
+        public ActionResult UserDetails()
+        {
+            //Link to SQL Database with UserDetailsViewModel
+            List<UserDetailsViewModel> userDetails = db.UserDetails.
+                Select(u => new UserDetailsViewModel()
+                {
+                    UserId = u.UserId,
+                    FirstName = u.FirstName,
+                    LastName = u.LastName
+                }).ToList<UserDetailsViewModel>();
             return View();
+        }
+
+        //GET
+        public ActionResult Create()
+        {
+            ViewBag.UserId = new SelectList(db.UserDetails, "UserId", "UserDetails");
+            return View();
+        }
+
+        //POST: UserDetails
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "UserId, FirstName, LastName")] UserDetail userDetail)
+        {
+            if (ModelState.IsValid)
+            {
+                //Add a new UserDetail to the database with a View if Model is valid
+                db.UserDetails.Add(userDetail);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            //Grabbing the newly entered data and showing it to the screen.
+            ViewBag.OwnerAssetId = new SelectList(db.UserDetails, "UserId", "UserId", userDetail.UserId);
+            return View(userDetail);            
         }
     }
 }
